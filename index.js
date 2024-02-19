4dda64a1cdb312e6d6439449f2187370d5b74849
Jebosa Samson <Jebosasamson7@gmail.com>
2024-02-19 12:56:13 -0800

    first respo

---
+++ index.js
@@ -0,0 +1,67 @@
+//Variable declaration
+let fistName = "Samson"
+let anotherAge = 30
+let city = "Asaba"
+let isStudent = true
+
+
+// Object
+let person = {
+    firstName: "Samson O",
+    age: 30,
+    city: "Okpanam",
+    isStudent: false,
+}
+
+// console.log(person)
+
+// dot notation
+// console.log(person.age)
+// console.log(person.city)
+// console.log(person.firstName)
+// console.log(person.isStudent)
+
+// square bracket
+// console.log(person['firstName'])
+// console.log(person['age'])
+// console.log(person['city'])
+// console.log(person['isStudent'])
+
+// Array
+let colors = ['red', 'green', 30, true]
+
+// console.log(colors[4])
+
+// Function
+function greet(name) {
+    let welcomeMsg = "Hello! My name is "
+    console.log(welcomeMsg + name)
+}
+
+// greet('John')
+// greet('Joy')
+// greet('Sam')
+// greet('Great')
+// greet('Promise')
+// greet('Faith')
+
+
+//Operators in JavaScript
+
+//Arithmetic Operators
+let sum = 2 + 2 //Addition
+let difference = 5 - 1 //Subtraction
+let division = 4 / 2 // division
+let multiplication = 4 * 2 //multiplication
+let modulus = 9 % 2 //modulus
+
+// Comparison Operators
+
+let equalTo = sum === difference // equal to
+let notEqualTo = sum !== difference //not equal to
+let greaterThan = sum > difference
+
+//Logical Operators
+
+
+console.log(greaterThan)
